8d7048a3f46d3efcf9b861a7840ec9bebf4c61df
Sourabh Sethi <sourabhhsethii@linux.com>
2022-01-04 15:12:51 -0500

    @Profile Anotation & Profiling Spring boot Application.

--- src/main/java/com/upgrad/MovieappApplication.java
+++ src/main/java/com/upgrad/MovieappApplication.java
@@ -2,15 +2,21 @@ package com.upgrad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestController
 @SpringBootApplication
 public class MovieappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieappApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MovieappApplication.class, args);
+		System.out.println(context.getBeanDefinitionCount());
+		System.out.println(Arrays.stream(context.getBeanDefinitionNames()).collect(Collectors.toList()).contains("myService"));
 		System.out.println("Hello Team");
 	}
 
